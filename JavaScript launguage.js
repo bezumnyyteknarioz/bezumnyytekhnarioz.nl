30ad7864ca4a7c94dfcfa4657dab6f4cb7658e44
bezumnyyteknarioz <alcheev.com@gmail.com>
2024-11-18 23:25:15 +0100

    Delete JavaScript launguage.js

--- JavaScript launguage.js
+++
@@ -1,8 +0,0 @@
-function changeLanguage() {
-    const language = document.getElementById('language').value;
-    if (language === "en") {
-        document.querySelector("header nav ul li:nth-child(1) a").innerText = "Home";
-        // Update all other sections' text for English
-    }
-    // Similar logic for other languages
-}
